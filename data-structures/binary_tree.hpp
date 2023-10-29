63866947d5549580b00a9a507ad95c7cfb3b4447
Cédric Agoliki <agolikicedric@gmail.com>
2023-10-29 19:59:13 +0000

    add constructor and destructor

--- data-structures/binary_tree.hpp
+++ data-structures/binary_tree.hpp
@@ -1,7 +1,18 @@
 template<typename T> class binary_tree {
+
+  public:
   struct node {
     node* left;
     node* right;
     typename value;
   }
+
+  binary_tree() {
+    root = NULL;
+  }
+
+  ~binary_tree() {}
+
+  private:
+  node* root;
 }
